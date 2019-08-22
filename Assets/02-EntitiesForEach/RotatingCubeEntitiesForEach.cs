06bc831503155f2da13666b3e5178b5f50f5ed35
Dale Kim <dale@unity3d.com>
2019-08-22 14:33:58 -0700

    Cubes being spawned.

--- Assets/02-EntitiesForEach/RotatingCubeEntitiesForEach.cs
+++ Assets/02-EntitiesForEach/RotatingCubeEntitiesForEach.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 public class RotatingCubeEntitiesForEach : ComponentSystem
@@ -19,5 +21,24 @@ public class RotatingCubeEntitiesForEach : ComponentSystem
 
     protected override void OnUpdate()
     {
+        Entities.ForEach((Entity entity, ref RotatingCubeSpawnerData spawnerData) =>
+        {
+            for (int i = 0; i < spawnerData.NumCubes; ++i)
+            {
+                float rad = ((float)i / (float)spawnerData.NumCubes) * Mathf.PI * 2.0f;
+                float posX = spawnerData.SpawnRadius * math.sin(rad);
+                float posZ = spawnerData.SpawnRadius * math.cos(rad);
+
+                // Actually create a rotating cube entity from the prefab.
+                var rotatingCubeEntity = EntityManager.Instantiate(spawnerData.RotatingCubePrefabEntity);
+
+                // Set the position of the rotating cube.
+                EntityManager.SetComponentData(rotatingCubeEntity, new Translation { Value = new float3(posX, 0.0f, posZ) });
+            }
+
+            // We should destroy this spawner entity because if we don't, the spawner will run again on the next frame
+            // and spawn another NumCubes rotating cubes!
+            EntityManager.DestroyEntity(entity);
+        });
     }
 }
