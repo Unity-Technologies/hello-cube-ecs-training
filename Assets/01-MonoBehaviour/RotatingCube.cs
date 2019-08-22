6d891e406395a2a7f69a482cc3afe427020eecbf
Dale Kim <dale@unity3d.com>
2019-08-21 18:40:48 -0700

    Setting up RotatingCubePrefab.

--- Assets/01-MonoBehaviour/RotatingCube.cs
+++ Assets/01-MonoBehaviour/RotatingCube.cs
@@ -2,16 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeSpawner : MonoBehaviour
+public class RotatingCube : MonoBehaviour
 {
-    public int NumCubes;
-
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < NumCubes; ++i)
-        {
-        }
+        
     }
 
     // Update is called once per frame
